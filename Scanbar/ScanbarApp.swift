9b1d536ddefd4815a5127753994d0287ae6a9142
Jan Smolinski <jassmolinski@gmail.com>
2022-07-28 07:13:20 +0200

    Initial Commit

---
+++ Scanbar/ScanbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScanbarApp.swift
+//  Scanbar
+//
+//  Created by Jan Smolinski on 28/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScanbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
